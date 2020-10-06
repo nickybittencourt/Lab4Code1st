b97ed9bfd4b034acb418fdea87a85ee64ac3b273
nickybittencourt <nickytuon75@gmail.com>
2020-10-06 17:28:49 -0300

    fixing connection errors

--- Startup.cs
+++ Startup.cs
@@ -29,11 +29,11 @@ namespace Lab4
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("Default")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-           services.AddRazorPages();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
